6767ef8fa0b1d8c59a4f924a28a678e85b78e19c
Davidetom <164945558+sasbura@users.noreply.github.com>
2024-04-12 15:51:00 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/giocatore/Giocatore.java
@@ -0,0 +1,38 @@
+package it.uniroma3.diadia.giocatore;
+
+/**
+ * Classe che gestisce i CFU del giocatore e di memorizzare gli attrezzi
+ *  presenti nella borsa
+ * @see Borsa
+ * @version Base
+ */
+
+public class Giocatore {
+	
+	static final private int CFU_INIZIALI = 20;
+	public int cfu;
+	public Borsa borsa;
+	
+	public Giocatore() {
+		this.cfu = CFU_INIZIALI;
+		this.borsa = new Borsa();
+	}
+	
+	public int getCfu() {
+		return this.cfu;
+	}
+	
+	public void setCfu(int cfu) {
+		this.cfu = cfu;
+	}
+	
+	public String mostraInventario() {
+		return this.borsa.toString();
+	}
+	
+	public Borsa getBorsa() {
+		return this.borsa;
+	}
+	
+	
+}
